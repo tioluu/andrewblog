18626c706dbc86931aa47be285e4c7e652e9f46a
andrewk0l <andyoged92@gmail.com>
2024-08-28 00:30:10 +0100

    created an header/navbar

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Blog from './components/Blogs/Blog';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <main>
-    <Navbar/>
+      <Navbar/>
+      <Blog/>
     </main>
   );
 }
--- blog/src/components/navbar/Navbar.js
+++ blog/src/components/navbar/Navbar.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import './Navbar.css'
+import mouthlogo from '../../assets/mouthlogo.png'
 
 
 const Navbar = () => {
   return (
-      <header>
-        <a href='' className='logo'>raMble</a>
-        <div className='abc'>
+    <header>
+      <div className='logo'>
+        <img src={mouthlogo} alt=''/>
+        <a href='' className='logo-text'>raMbleonAndrew</a>
+      </div>
+      <div className='abc'>
 
-          <span>About</span>
-          <span>Blog</span>
-          <span>Contact</span>
-        </div>
-        <nav>
-          <a href=''>Login</a>
-          <a href=''>Sign up</a>
-        </nav>
+        <span>About</span>
+        <span>Blog</span>
+        <span>Contact</span>
+      </div>
+      <nav>
+        <a href=''>Login</a>
+        <a href=''>Sign up</a>
+      </nav>
 
-      </header>
+    </header>
   )
 }
 
