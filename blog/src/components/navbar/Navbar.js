3b23eabc9b968ed618861e18c3ebad3ed2941e35
andrewk0l <andyoged92@gmail.com>
2024-08-30 02:39:04 +0100

    adding responsiveness to navbar

--- blog/src/components/navbar/Navbar.js
+++ blog/src/components/navbar/Navbar.js
@@ -1,16 +1,30 @@
 import React from 'react'
+import { useState } from 'react';
 import './Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import mouthlogo from '../../assets/mouthlogo.png'
 
 
 const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-        <div className='navbar-brand'>
-        <img src={mouthlogo} alt=''/>
+      <div className='navbar-brand'>
+        <img src={mouthlogo} alt='' />
         <a href='' className='logo-text'>raMbleonAndrew</a>
       </div>
-      <ul className="navbar-nav">
+
+      <div className="menu-icon" onClick={toggleMenu}>
+      <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+      </div>
+      <ul className={`navbar-nav ${isOpen ? 'active' : ''}`}>
         <li className="nav-item">
           <a href="/">Home</a>
         </li>
