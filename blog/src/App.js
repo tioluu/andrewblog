66c888f9c501b5f5119c26d644a4bc1907c54c4e
andrewk0l <andyoged92@gmail.com>
2024-08-30 13:11:06 +0100

    created and styled footer

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Blog from './components/Blogs/Blog';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <main>
       <Navbar/>
       <Blog/>
+      <Footer/>
     </main>
   );
 }
---
+++ blog/src/components/footer/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter, faSource, faSquareLetterboxd, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import './Footer.css'; // Import the CSS file for styling
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>© 2024 raMbleonAndrew. All rights reserved.</p>
+        <div className="social-links">
+          <a href="https://github.com/andrewk0l" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="lg" />
+          </a>
+          <a href="https://x.com/natkingkol_" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faXTwitter} size="lg" />
+          </a>
+          <a href="https://letterboxd.com/corinthian_/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faSquareLetterboxd} size="lg" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
