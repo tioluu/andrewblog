28f1180daa87914177f9ca0fd1dcc882835682de
Tiolu <tholuu19@gmail.com>
2025-02-06 23:33:31 +0100

    added database

---
+++ database/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE contacts (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
