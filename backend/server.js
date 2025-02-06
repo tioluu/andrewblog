d219694017df5ff48cb3684a6ca57f4469bfb8e3
Tiolu <tholuu19@gmail.com>
2025-02-07 00:35:02 +0100

    refactor code

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const cors = require("cors");
+const app = express();
+
 app.use(cors({ origin: "http://localhost:3000" }));
 
-const app = express();
+
 app.use(express.json());
 
 const pool = new Pool({
